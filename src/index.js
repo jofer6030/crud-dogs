15134f681f6f00b545f9807b6bf3a898073b94ba
Jose Fernando <jofer0628@gmail.com>
2021-10-06 20:25:17 -0500

    carpeta build fixed again

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   },
 };
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin : *");
 
   res.header(
     "Access-Control-Allow-Headers",
